c27b3fea1dacc2d342c87e822c9270dec6594c05
kowalikw <kowalikw.mini@gmail.com>
2016-05-10 17:36:26 +0200

    Read file.

---
+++ AZ/FileHelper.cs
@@ -0,0 +1,33 @@
+﻿using ASD.Graphs;
+using System.IO;
+
+namespace AZ
+{
+    public static class FileHelper
+    {
+        public static Graph LoadFile(string path)
+        {
+            Graph graph;
+
+            using (StreamReader file = new StreamReader(path))
+            {
+                string line;
+                int verticesCount = int.Parse(file.ReadLine());
+
+                graph = new AdjacencyMatrixGraph(false, verticesCount);
+                while ((line = file.ReadLine()) != null)
+                {
+                    var persons = line.Split(',');
+                    graph.AddEdge(int.Parse(persons[0]), int.Parse(persons[1]), 1);
+                }
+            }
+
+            return graph;
+        }
+
+        public static void SaveFile()
+        {
+
+        }
+    }
+}
--- AZ/MainWindow.xaml.cs
+++ AZ/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AZ
 {
@@ -23,6 +12,27 @@ namespace AZ
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    FileHelper.LoadFile(openFileDialog.FileName);
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("");
+                }
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
