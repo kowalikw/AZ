f621a8a0cde44c6571f798380e71d26067290c59
Konrad Miśkiewicz <miskiewicz.konrad@gmail.com>
2016-05-30 21:32:05 +0200

    testy

--- AZ_Tests/UnitTest1.cs
+++ AZ_Tests/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using ASD.Graphs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AZ;
+using System.Collections.Generic;
 
 namespace AZ_Tests
 {
@@ -7,8 +10,65 @@ namespace AZ_Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FileReaderTest()
         {
+            Graph g = FileHelper.LoadFile("test.txt");
+
+            Assert.AreEqual(g.VerticesCount, 5);
+            Assert.AreEqual(g.EdgesCount, 7);
+            List<Edge> lst = new List<Edge>();
+            for (int i = 0; i < 5; i++)
+                foreach (Edge e in g.OutEdges(i))
+                    lst.Add(e);
+
+            Assert.IsTrue(lst.Contains(new Edge(0, 1)));
+            Assert.IsTrue(lst.Contains(new Edge(0, 2)));
+            Assert.IsTrue(lst.Contains(new Edge(0, 3)));
+            Assert.IsTrue(lst.Contains(new Edge(0, 4)));
+            Assert.IsTrue(lst.Contains(new Edge(3, 1)));
+            Assert.IsTrue(lst.Contains(new Edge(4, 1)));
+            Assert.IsTrue(lst.Contains(new Edge(2, 3)));
+        }
+
+
+        [TestMethod]
+        public void LineGraphTest()
+        {
+            Graph g = FileHelper.LoadFile("test.txt");
+            List<Edge> lst;
+            Graph k = GraphHelper.LineGraph(g, out lst);
+            Assert.AreEqual(14, k.EdgesCount);
+            Assert.AreEqual(7, k.VerticesCount);
+        }
+
+
+        [TestMethod]
+        public void ComplementGraphTest()
+        {
+            Graph g = FileHelper.LoadFile("test.txt");
+
+            Graph k = GraphHelper.ComplementGraph(g);
+
+            List<Edge> lst = new List<Edge>();
+            for (int i = 0; i < 5; i++)
+                foreach (Edge e in k.OutEdges(i))
+                    lst.Add(e);
+            for (int i = 0; i < 5; i++)
+                foreach (Edge e in g.OutEdges(i))
+                    Assert.IsFalse(lst.Contains(e));
+            Assert.AreEqual(3, k.EdgesCount);
+        }
+
+
+        [TestMethod]
+        public void FindMaximumMatchingTest()
+        {
+            Graph g = FileHelper.LoadFile("test.txt");
+            List<Edge> lst, lst1 = new List<Edge>();
+            Graph k = GraphHelper.LineGraph(g, out lst);
+            k = GraphHelper.ComplementGraph(k);
+            lst = GraphHelper.FindMaximumMatching(k, lst1);
+            Assert.AreEqual(lst.Count, (int)(k.VerticesCount/2));
         }
     }
 }
